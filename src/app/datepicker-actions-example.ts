5dbd8f1bf9717a709e82bd20395d9d0210b15d7a
KMaster90 <73232282+KMaster90@users.noreply.github.com>
2023-02-20 13:15:28 +0100

    3 files modified, 1 added

--- src/app/datepicker-actions-example.ts
+++ src/app/datepicker-actions-example.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { first } from 'rxjs';
+import 'moment-timezone';
+import moment from 'moment';
+import { MatListItem } from '@angular/material/list';
 
 /** @title Datepicker action buttons */
 @Component({
@@ -9,20 +12,39 @@ import { first } from 'rxjs';
   styleUrls: ['datepicker-actions-example.css'],
 })
 export class DatepickerActionsExample {
-  value='2022-02-10T09:50';
-  mySelectedDate={date:'',h:'',m:''}
-  hours = [...Array(24).keys()].map(x=>`${x}`.padStart(2,'0'));
-  minutes = [...Array(60).keys()].map(x=>`${x}`.padStart(2,'0'));
-
-  setDatePicker(ev?:any){
-    console.log(this.value)
-  this.mySelectedDate.date=ev?.value;
-    console.log(this.value, this.mySelectedDate)
+  @ViewChildren('hours') hoursItem: QueryList<MatListItem>;
+  @ViewChildren('minutes') minutesItem: QueryList<MatListItem>;
+  value = new Date().toString();
+  mySelectedDate = {
+    date: this.value,
+    h: new Date().getHours().toString(),
+    m: new Date().getMinutes().toString(),
+  };
+  hours = [...Array(24).keys()].map((x) => `${x}`.padStart(2, '0'));
+  minutes = [...Array(60).keys()].map((x) => `${x}`.padStart(2, '0'));
+  SHOP_TIMEZONE_OFFSET;
+  constructor() {
+    moment.tz.setDefault('America/New_York');
+    this.SHOP_TIMEZONE_OFFSET = moment(new Date()).format('ZZ');
   }
+
+  setDatePicker(ev?: any) {
+    this.mySelectedDate.date = ev?.value;
+    const stringDate = this.setDateTime(this.mySelectedDate);
+    this.setShopTimezone(stringDate);
+  }
+
+  setDateTime({ date, h, m }: typeof this.mySelectedDate) {
+    return new Date(
+      new Date(this.mySelectedDate.date).setHours(+h, +m)
+    ).toString();
+  }
+
+  setShopTimezone(stringDate: string) {
+    const regex = /^([A-Za-z]{3} \w{3} \d{2} \d{4} \d{2}:\d{2}:\d{2})/;
+    const match = regex.exec(stringDate);
+    this.value = `${match ? match[1] : null} GMT${this.SHOP_TIMEZONE_OFFSET}`;
+  }
+
+  scrollIntoView() {}
 }
-
-
-
-/**  Copyright 2023 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at https://angular.io/license */
