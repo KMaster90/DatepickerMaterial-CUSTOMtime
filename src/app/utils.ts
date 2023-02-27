a916dbb6b12131f92c7348955e9fb4a19756c5b6
KMaster90 <73232282+KMaster90@users.noreply.github.com>
2023-02-27 12:29:58 +0100

    GMT_NUMBER

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -3,6 +3,7 @@ export {};
 declare global {
   interface String {
     padClock: () => string[];
+    GMT_Number: () => string;
   }
 }
 
@@ -10,4 +11,10 @@ String.prototype.padClock = function () {
   return this.split('.').map((p, i) =>
     !i ? p.padStart(2, '0') : p.padEnd(2, '0')
   );
+};
+String.prototype.GMT_Number = function () {
+  const my_this = this as any as number;
+  const h = `${Math.abs(Math.floor(my_this / 60))}`.padStart(2, '0');
+  const m = `${Math.abs(my_this % 60)}`.padStart(2, '0');
+  return `${Math.sign(my_this) > 0 ? '+' : '-'}${h}:${m}`;
 };
